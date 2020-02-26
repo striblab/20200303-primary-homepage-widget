d57782cfd9423b564a74495e47d2cbd90fc32785
Thomas Oide <thomas.t.oide@gmail.com>
2020-02-26 16:30:34 -0600

    final styles

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,9 @@
 	let json = [];
 	let statewide_data = [];
 	let state_precincts_pct;
+	let top_five = [];
+	let i;
+
 	const date_options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'};
 
 	$ : {
@@ -17,16 +20,31 @@
 
 		if (statewide_data.length == 0) {
 			state_precincts_pct = '';
+			top_five = []
 		}
 		else {
 			state_precincts_pct = statewide_data[0].precinctsreportingpct;
+			top_five = statewide_data.slice(0,5);
 		}
 
-		// if (statewide_data.length > 0) {
-		// 	last_updated = new Date(statewide_data[0].lastupdated).toLocaleString('en-US', date_options)
-		// } else {
-		// 	last_updated = "Retrieving data..."
-		// }
+
+		if (top_five.some(elem => elem.last === 'Uncommitted') && Math.round(state_precincts_pct * 100) < 50) {
+			top_five = []
+			for (i = 0; i < 6; i++) {
+				if (statewide_data[i].last === 'Uncommitted') {
+
+					continue;
+				}
+				else {
+					top_five.push(statewide_data[i])
+				}
+			}
+		}
+		else {
+			top_five = [];
+			top_five = statewide_data.slice(0,5)
+		}
+		// console.log(top_five)
 
 	}
 
@@ -49,25 +67,26 @@
 
 </style>
 
-<!-- <a class="wrapperLink" href="http://startribune.com"> -->
+<a class="wrapperLink" href="http://startribune.com">
 <div id="widgetContainer">
 	<div class="logo">
 		<div class="text">
 			<h3>ELECTION</h3>
-			<a href="startribune.com">See full Minn. Primary results</a>
+			<a href="startribune.com">See Minnesota primary results</a>
 		</div>
 		<img src="img/ELEX2020.svg" alt="Election 2020"/>
-		<a class="rightLink" href="startribune.com">SEE FULL RESULTS</a>
+		<a class="rightLink" href="startribune.com">SEE MINNESOTA RESULTS</a>
 	</div>
 
 
 	<div class="candidateContainer">
-		{#each statewide_data as candidate, i}
-			{#if statewide_data[0].winner == true}
-				{#if i < 5}
+		{#each top_five as candidate, i}
+			{#if top_five[0].winner == true}
 				<div class="candidate">
 					{#if candidate.last !== 'Uncommitted'}
 					<img class="mug" src="img/{candidate.last.toLowerCase()}.jpg" alt="{candidate.last} photo"/>
+					{:else}
+					<span class="uncommitted"></span>
 					{/if}
 					<div class="results">
 						{#if candidate.winner == true}
@@ -85,9 +104,7 @@
 						<div class="votecount">Votes: {intcomma(candidate.votecount)}</div>
 					</div>
 				</div>
-				{/if}
 			{:else}
-				{#if i < 5}
 				<div class="candidate">
 					{#if candidate.last !== 'Uncommitted'}
 					<img class="mug" src="img/{candidate.last.toLowerCase()}.jpg" alt="{candidate.last} photo"/>
@@ -109,9 +126,8 @@
 					</div>
 				</div>
 				{/if}
-			{/if}
 		{/each}
 	</div>
 </div>
 	<p class="lastUpdated">{ Math.round(state_precincts_pct * 100) }% PRECINCTS REPORTING</p>
-<!-- </a> -->
+</a>
